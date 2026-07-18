4566e868a50069362dad7b76dee36f8fc2e29bdb
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-18 11:15:47 +0600

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:nnotes/screens/HomeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'nNotes',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: Homescreen(),
+    );
+  }
+}
