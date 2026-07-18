811e13f73a04ff131b8c0356529cc8fbd8b83e69
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-18 12:34:26 +0600

    simple splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 15,
+          children: [
+            Container(
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
+              ),
+              child: Center(
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "n",
+                        style: TextStyle(color: Colors.yellow, fontSize: 25),
+                      ),
+                      TextSpan(
+                        text: "Notes",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              "Keep your notes in nNotes...",
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
