85001147a41ed7ed39fba62c5e29783fc68ee7ac
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-19 22:21:19 +0600

    app title to notesNstudents

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'nNotes',
+      title: 'notesNstudents',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
