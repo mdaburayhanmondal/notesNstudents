f7c385be6c907915a80d25a83afefa75f7378df3
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-19 14:57:54 +0600

    added note details screen

---
+++ lib/screens/note_details_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:nnotes/data/notes.dart';
+
+class NoteDetailsScreen extends StatefulWidget {
+  final int postId;
+
+  const NoteDetailsScreen({super.key, required this.postId});
+
+  @override
+  State<NoteDetailsScreen> createState() => _NoteDetailsScreenState();
+}
+
+class _NoteDetailsScreenState extends State<NoteDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          spacing: 20,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 5,
+              children: [
+                Text(
+                  "Note Title:",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+                Text(
+                  notes[widget.postId]["title"].toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 5,
+              children: [
+                Text(
+                  "Note Description:",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+                Text(
+                  notes[widget.postId]["description"].toString(),
+                  style: TextStyle(fontSize: 15),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
