83890472c5cc7989eb83d31ea45984be2edad22b
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-07-22 16:16:46 +0600

    implemented realtime search

--- lib/screens/nStudents/all_students_screen.dart
+++ lib/screens/nStudents/all_students_screen.dart
@@ -12,6 +12,23 @@ class AllStudentsScreen extends StatefulWidget {
 }
 
 class _AllStudentsScreenState extends State<AllStudentsScreen> {
+  List<Student> get filteredStudents {
+    final searchedText = searchController.text.trim().toLowerCase();
+
+    if (searchedText.isEmpty) {
+      return students;
+    }
+
+    return students.where((s) {
+      return s.firstName.toLowerCase().contains(searchedText) ||
+          s.lastName.toLowerCase().contains(searchedText) ||
+          s.email.toLowerCase().contains(searchedText) ||
+          s.department.toLowerCase().contains(searchedText);
+    }).toList();
+  }
+
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +47,7 @@ class _AllStudentsScreenState extends State<AllStudentsScreen> {
                   AddOrEditStudentScreen(addStudent: true, editStudent: false),
             ),
           );
-          if (newUser) {
+          if (newUser == true) {
             setState(() {});
           }
         },
@@ -49,121 +66,143 @@ class _AllStudentsScreenState extends State<AllStudentsScreen> {
                 ),
               ),
             )
-          : ListView.builder(
-              itemCount: students.length,
-              itemBuilder: (context, idx) => Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
+          : Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: searchController,
+                    onChanged: (_) => setState(() {}),
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Search student...",
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
                 ),
-                child: Row(
-                  spacing: 10,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  StudentDetailsScreen(sId: students[idx].id),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          width: double.infinity,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.orange,
-                            borderRadius: BorderRadiusGeometry.circular(10),
-                          ),
-                          child: Center(
-                            child: Text(
-                              "${students[idx].id}",
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white,
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: filteredStudents.length,
+                  itemBuilder: (context, idx) {
+                    final student = filteredStudents[idx];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      child: Row(
+                        spacing: 10,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        StudentDetailsScreen(sId: student.id),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                width: double.infinity,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: Colors.orange,
+                                  borderRadius: BorderRadiusGeometry.circular(
+                                    10,
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "${student.id}",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 7,
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  StudentDetailsScreen(sId: students[idx].id),
-                            ),
-                          );
-                        },
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "${students[idx].firstName} ${students[idx].lastName}",
-                              style: TextStyle(fontSize: 15),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                            Text(
-                              students[idx].email,
-                              style: TextStyle(fontSize: 12),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          InkWell(
-                            onTap: () async {
-                              final updated = await Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => AddOrEditStudentScreen(
-                                    sId: students[idx].id,
-                                    editStudent: true,
+                          Expanded(
+                            flex: 7,
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        StudentDetailsScreen(sId: student.id),
                                   ),
-                                ),
-                              );
-                              if (updated == true) {
-                                setState(() {});
-                              }
-                            },
-                            child: Icon(Icons.edit_note),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              stuSnackbar(
-                                context,
-                                "${students[idx].firstName} ${students[idx].lastName} is deleted",
-                                bgColor: Colors.red,
-                              );
-                              setState(() {
-                                students.removeWhere(
-                                  (s) => s.id == students[idx].id,
                                 );
-                              });
-                            },
-                            child: Icon(Icons.delete, color: Colors.red),
+                              },
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${student.firstName} ${student.lastName}",
+                                    style: TextStyle(fontSize: 15),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                  Text(
+                                    student.email,
+                                    style: TextStyle(fontSize: 12),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                InkWell(
+                                  onTap: () async {
+                                    final updated = await Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => AddOrEditStudentScreen(
+                                          sId: student.id,
+                                          editStudent: true,
+                                        ),
+                                      ),
+                                    );
+                                    if (updated == true) {
+                                      setState(() {});
+                                    }
+                                  },
+                                  child: Icon(Icons.edit_note),
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    stuSnackbar(
+                                      context,
+                                      "${student.firstName} ${student.lastName} is deleted",
+                                      bgColor: Colors.red,
+                                    );
+                                    setState(() {
+                                      students.removeWhere(
+                                        (s) => s.id == student.id,
+                                      );
+                                    });
+                                  },
+                                  child: Icon(Icons.delete, color: Colors.red),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
-              ),
+              ],
             ),
     );
   }
